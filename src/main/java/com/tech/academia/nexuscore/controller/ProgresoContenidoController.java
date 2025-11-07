5ae5941442663c8e94de8e9066f35674a9a5cf76
ki3anecromancer <ki.3a@hotmail.com>
2025-11-07 13:11:32 -0300

    implementado ProgresoContenidoController

--- src/main/java/com/tech/academia/nexuscore/controller/ProgresoContenidoController.java
+++ src/main/java/com/tech/academia/nexuscore/controller/ProgresoContenidoController.java
@@ -1,11 +1,14 @@
 package com.tech.academia.nexuscore.controller;
 
 import com.tech.academia.nexuscore.dto.ProgresoContenidoResponseDTO;
+import com.tech.academia.nexuscore.security.userdetails.UsuarioDetails;
 import com.tech.academia.nexuscore.service.ProgresoContenidoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,12 +19,13 @@ public class ProgresoContenidoController {
 
   private final ProgresoContenidoService progresoContenidoService;
 
-  // TODO: FINALIZAR DESPUÉS DE SPRING SECURITY JWT
   // Marcar contenido como completado
   @PatchMapping("/{idContenido}/completado")
   public ResponseEntity<ProgresoContenidoResponseDTO> marcarContenidoComoCompletado(
-      Long idUsuario,
-      Long idContenido) {
+      @PathVariable Long idContenido,
+      @AuthenticationPrincipal UsuarioDetails usuarioDetails) {
+
+    Long idUsuario = usuarioDetails.getId();
 
     return ResponseEntity
         .status(HttpStatus.OK)
